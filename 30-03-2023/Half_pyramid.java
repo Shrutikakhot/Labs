ff8c8761b87192631749300f0fc8a17d6c9fe4e5
Shrutikakhot <101885694+Shrutikakhot@users.noreply.github.com>
2023-03-30 17:57:56 +0530

    Create Half_pyramid.java

---
+++ 30-03-2023/Half_pyramid.java
@@ -0,0 +1,18 @@
+package practiceCode;
+
+public class Half_pyramid {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int row=4;
+		System.out.println("Half Pyramid");
+		for(int i=1; i<=4; i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}	
+	}
+}
