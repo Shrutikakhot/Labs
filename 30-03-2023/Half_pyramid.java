974d25df5b878ac822db6ec96c7aed9733356c8d
Shrutikakhot <101885694+Shrutikakhot@users.noreply.github.com>
2023-03-30 21:03:10 +0530

    Update Half_pyramid.java

--- 30-03-2023/Half_pyramid.java
+++ 30-03-2023/Half_pyramid.java
@@ -1,14 +1,13 @@
 package practiceCode;
 
-public class Half_pyramid {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int row=4;
-		System.out.println("Half Pyramid");
-		for(int i=1; i<=4; i++)
+public class Half_pyramid 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Half Pyramid");//printing message
+		for(int i=1; i<=4; i++)//outer loop
 		{
-			for(int j=1;j<=i;j++)
+			for(int j=1;j<=i;j++)//inner loop
 			{
 				System.out.print("* ");
 			}
