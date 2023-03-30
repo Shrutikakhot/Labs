b1fae1fe784de5aac370be5624857884a16316f4
Shrutikakhot <101885694+Shrutikakhot@users.noreply.github.com>
2023-03-30 21:06:04 +0530

    Update Armstrong_no.java

--- 30-03-2023/Armstrong_no.java
+++ 30-03-2023/Armstrong_no.java
@@ -1,28 +1,26 @@
 package practiceCode;
 public class Armstrong_no
 {
-
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
 		for(int j=0;j<=1000;j++)
 		{
 			arms(j);
 		}
-        
 	}
-	static void arms(int n){
-		int arm_no=0,rem,a;
+	static void arms(int n)
+	{
+		int arm_no=0, rem, a;
 		a = n;
-        while(n>0)
-        {
-            rem = n%10;
-            arm_no = (rem*rem*rem)+arm_no;
-            n=n/10;
-        }
-        if(arm_no==a)
-        {
-            System.out.println(a);
-        }
+        		while(n>0)
+        		{
+           		   rem = n%10;
+           		   arm_no = (rem*rem*rem)+arm_no;
+           		   n=n/10;
+       			}
+        		if(arm_no==a)
+        		{
+         		   System.out.println(a);
+       			}
 	}
 }
